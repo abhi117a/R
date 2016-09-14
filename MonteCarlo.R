e591b25673096a35995027ca88656b1c67862281
abhi117a <abhi_9trox@yahoo.co.uk>
2016-09-14 14:26:34 -0500

    Monte Carlo Simulation of Random variables having Gamma distribution. Using Replicate function for repeating the simulation for 10000times

--- MonteCarlo.R
+++ MonteCarlo.R
@@ -1,6 +1,10 @@
 gammaSample <- replicate(10000, (max(rgamma(3, 1, 5)))) #Creating Random Variables of Gamma distribution.
 draws<- 1/gammaSample #Converting Lambda into Time.
+curve(dgamma(x,1,5),add = FALSE,col = 'blue') #Plot Curve 
+hist(draws) #Plot the histogram
 expectedValue <- mean(draws) #Getting Expected Value
+print (expectedValue) #Printing the expected Value(Mean)
+#Logic for counting the mean for time > 20mins
 count <- 0
 for (v in draws)
 {
@@ -8,9 +12,7 @@ for (v in draws)
      count <- count +1
      }
 }
-probablity <- count/10000
-print (probablity)
+probablity <- count/10000 #Generating Expected Value
+print (probablity) #Printing the Expected Value
 
-#hist(draws)
-#curve(dgamma(x,1,5),add = TRUE,col = 'blue')
 
