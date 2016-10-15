39b418b3628cf0e17e9e0af8e18d2ab09b5ddab3
abhi117a <abhi_9trox@yahoo.co.uk>
2016-10-15 15:51:02 -0500

    Coordinates and Facets

--- MovieRatings.r
+++ MovieRatings.r
@@ -39,3 +39,86 @@ s+geom_histogram(binwidth = 10,aes(fill=Genere),color="Black")
 s+geom_density(aes(fill=Genere))
 
 s+geom_density(aes(fill=Genere),position = "stack")
+
+
+#------Starting Layer Tips
+
+t <- ggplot(data=movies,aes(x=AudienceRating))
+t+geom_histogram(binwidth = 10,fill = "Black",color ="REd")
+
+#different way
+
+t <- ggplot(data=movies)
+t+geom_histogram(binwidth = 5,aes(x=AudienceRating),fill="Black",color ="Green")
+
+#hist for critiq rating
+
+t <- ggplot(data=movies)
+t+geom_histogram(binwidth = 5,aes(x=CritiqueRating),fill="Pink",color ="Black")
+
+
+#Geom_Smooth
+
+t <- ggplot(data=movies,aes(x=CritiqueRating,y=AudienceRating,color = Genere)) 
+
+t + geom_point() + geom_smooth(fill=NA)
+
+
+#boxPlots
+
+u <- ggplot(data=movies,aes(x=Genere, y=AudienceRating,color= Genere))
+
+u+geom_boxplot(size =1.2)
+
+#Jitter
+
+u+geom_boxplot(size =1.2) + geom_jitter()
+
+#jitter way 2
+
+u+geom_jitter()+geom_boxplot(size =1.2, alpha=0.5)
+
+#CritiqueRating BoxPlot
+
+g <- ggplot(data = movies,aes(x=Genere,y=CritiqueRating, color = Genere))
+
+g+geom_boxplot() + geom_jitter()
+
+g+geom_jitter()+geom_boxplot(alpha=0.5)
+
+
+#using facet
+
+f <- ggplot(data= movies, aes(x=BudgetMillions))
+f+geom_histogram(binwidth = 10,aes(fill = Genere),color = "Black") +
+  facet_grid(Genere~., scales="free")
+
+#ScatterPlot
+
+s <- ggplot(data = movies,aes(x=CritiqueRating,y=AudienceRating,color=Genere))
+s+geom_point(size=3)
+
+#facet
+
+s+geom_point(size=3) + facet_grid(Genere~.)
+
+s+geom_point(size=3) + facet_grid(.~Year)
+
+s+geom_point(size=3) + facet_grid(Genere~Year)
+
+s+geom_point(size=3) + facet_grid(Genere~Year) + geom_smooth()
+
+s+geom_point(aes(size=BudgetMillions)) + facet_grid(Genere~Year) + geom_smooth()
+
+#------Coordinates
+
+m <- ggplot(data=movies,aes(x=CritiqueRating,y=AudienceRating,color=Genere,size=BudgetMillions))
+
+m+geom_point()+xlim(50,100)+ylim(50,100)
+
+n <- ggplot(data = movies,aes(x=BudgetMillions))
+n + geom_histogram(binwidth = 10,aes(fill=Genere),color = "Black") + coord_cartesian(ylim = c(0,50))
+
+s <- ggplot(data = movies,aes(x=CritiqueRating,y=AudienceRating,color=Genere))
+
+s+geom_point(aes(size=BudgetMillions)) + facet_grid(Genere~Year) + geom_smooth() + coord_cartesian(ylim = c(0,100))
