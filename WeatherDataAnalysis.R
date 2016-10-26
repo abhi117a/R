1fa8339fa77b3240b9abfa07d04a8ffa594e2b46
abhi117a <abhi_9trox@yahoo.co.uk>
2016-10-26 03:59:24 -0500

    Implementing apply, sapply for user defined functions and predefined functions like rowMeans

--- WeatherDataAnalysis.R
+++ WeatherDataAnalysis.R
@@ -23,3 +23,30 @@ rbind( apply(Chicago, 1, mean),
        apply(NewYork, 1, mean),
        apply(Houston, 1, mean),
        apply(SanFrancisco, 1, mean))
+
+lapply(Weather, t)
+
+lapply(Weather, rbind, NewRow = 1:12)
+
+lapply(Weather, rowMeans)
+
+#######
+
+lapply(Weather, "[",1,1)
+
+lapply(Weather, "[",1,)
+
+lapply(Weather, "[",,3)
+
+##Apply ownFunction
+
+lapply(Weather, function(z) z[1,]-z[2,])
+lapply(Weather, function(z) round((z[1,]-z[2,])/z[2,],2))
+Weather
+lapply(Weather, function(x) round(x[4,]/x[3,],2))
+
+###################
+
+sapply(Weather, rowMeans)
+sapply(Weather, function(x) x[1,]-x[2,]/x[2,])
+sapply(Weather, function(z) round(z[4,]/z[3,],2))
