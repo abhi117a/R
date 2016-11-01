a5e538cdd8d26fa1618adc94a69070606c25550c
abhi117a <abhi_9trox@yahoo.co.uk>
2016-11-01 18:56:22 -0500

    Implementation of Backward Elimination method

--- MultivariateRegression.R
+++ MultivariateRegression.R
@@ -12,7 +12,19 @@ split = sample.split(Y = startUps$Profit, SplitRatio = 0.8)
 
 traing_set = subset(startUps, split == T)
 test_set = subset(startUps, split == F)
+#traing_set
+#test_set
+regressor = lm(formula = Profit ~.,data=traing_set)
+
+summary(regressor)
+#Predicitng 
+
+y_pred = predict(regressor,newdata = test_set)
+y_pred
+
+library(ggplot2)
+g <- ggplot(data= test_set)
+g+geom_point(aes(x=test_set$R.D.Spend,y=test_set$Profit), color = "Blue") +
+  geom_line(aes(x=test_set$R.D.Spend,y=y_pred),color = "Red")
 
 
-traing_set
-test_set
