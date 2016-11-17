657e24ed2072dfa1496b07383a4aa39b1a31c7be
abhi117a <abhi_9trox@yahoo.co.uk>
2016-11-17 17:20:10 -0600

    implementation of linear regression on Kaggle Dataset DataSet: https://archive.ics.uci.edu/ml/datasets/Student+Performance

---
+++ LinearRegressionOnKaggleDataSet.R
@@ -0,0 +1,20 @@
+#Liner Regression on Kaggle DataSet
+
+stud_mat <- read.csv("student-mat.csv", sep = ";")
+head(stud_mat)
+summary(stud_mat)
+str(stud_mat)
+#Check if dataset has null value
+any(is.na(stud_mat))
+library(ggplot2)
+library(ggthemes)
+library(dplyr)
+library(corrgram)
+library(corrplot)
+num_cols <- sapply(stud_mat,is.numeric)
+cor_data <- cor(stud_mat[,num_cols])
+#Correlation Plot
+corrplot(cor_data, method = "color")
+corrgram(stud_mat)
+corrgram(stud_mat, order = T, lower.panel = panel.shade, upper.panel = panel.pie, text.panel = panel.txt)
+ggplot(stud_mat, aes(x = stud_mat$G3)) + geom_histogram(color = "blue", fill = "red")
