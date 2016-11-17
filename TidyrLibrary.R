8222dabc4ec8907685dbf2babb3bf5c0550bac73
abhi117a <abhi_9trox@yahoo.co.uk>
2016-11-17 14:28:40 -0600

    trying Tidyr Library

---
+++ TidyrLibrary.R
@@ -0,0 +1,27 @@
+#install.packages("tidyr")
+#install.packages("data.table")
+library(tidyr)
+#gather
+comp <- c(1,1,1,2,2,2,3,3,3)
+yr <- c(1998,1999,2000,1998,1999,2000,1998,1999,2000)
+q1 <- runif(9, min=0, max=100)
+q2 <- runif(9, min=0, max=100)
+q3 <- runif(9, min=0, max=100)
+q4 <- runif(9, min=0, max=100)
+
+df <- data.frame(comp=comp,year=yr,Qtr1 = q1,Qtr2 = q2,Qtr3 = q3,Qtr4 = q4)
+gather(df, Quarter, Revenue,Qtr1:Qtr4)
+df %>%gather(Quarter,Revenue,Qtr1:Qtr4)
+stocks <- data.frame(
+  time = as.Date('2009-01-01') + 0:9,
+  X = rnorm(10, 0, 1),
+  Y = rnorm(10, 0, 2),
+  Z = rnorm(10, 0, 4)
+)
+stocks
+
+stocks_gathered <- stocks %>% gather(stock, price, X:Z)
+
+stocks_gathered %>% spread(stock, price)
+
+t.test
