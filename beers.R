51e77018e45702c1065c996252b60e43ecacfb95
abhi117a <abhi_9trox@yahoo.co.uk>
2017-03-08 23:58:17 -0600

    Implemented K-fold Cross validation

--- beers.R
+++ beers.R
@@ -11,16 +11,12 @@ beers[is.na(beers$abv), "abv"] <- meadianABV
 medianIbu <- median(beers$ibu, na.rm = T)
 beers[is.na(beers$ibu),"ibu"] <- medianIbu
 
-temp <- as.data.frame(cbind(beers$abv,beers$ibu))
-head(temp)
-head(temp)
-library(corrgram)
-corrgram(temp)
 
 library(ggplot2)
 b <- ggplot(beers)
 b+ geom_point(aes(x = beers$abv,y=beers$ibu, size = beers$ounces, 
-                  color = as.factor(beers$ounces)), alpha = 0.7)
+                  color = as.factor(beers$ounces)), alpha = 0.7) +
+              ggtitle("Alcohol Content vs Bitterness") + xlab("Alcohol Content")+ ylab( "Bitterness")
 #We can see a positive corelation between bitterness and alcohol content.
 #Let's perform linear regression on this as we can see only 2 variables are the most significant
 
@@ -33,16 +29,28 @@ beersTest <- subset(beers, split == FALSE)
 nrow(beersTest)
 lmBeers <- glm(formula = abv~ibu, data = beersTrain, family = "gaussian")
 summary(lmBeers)
+nrow(beers)
+MSE <- mean(lmBeers$residuals^2)
 
 predY <- predict(lmBeers, newdata = beersTest[,-1])
 summary(predY)
 
+mean((predY-beersTest[,1])^2)
+
+#Visualizing the results
 
 g <- ggplot()
 g + geom_point(aes(x= beersTest$ibu, y = beersTest$abv),
                color = "blue") +
-  geom_line(aes(x = beersTest$ibu, y = predY), color = "red")
+  geom_line(aes(x = beersTest$ibu, y = predY), color = "red") +
+  ggtitle("Alcohol Content vs Bitterness of Test set") + xlab("Alcohol Content")+ ylab( "Bitterness")
 
-cm <- table(predY,beersTest[,1])
+#Using Cross Validation Technique
+library(boot)
+cv.error = rep (0 ,10)
+  for (i in 1:10) {
+   glm.fit=glm(abv~ibu,data=beers)
+  cv.error[i]=cv.glm(beers ,glm.fit ,K=10) $delta [1] }
 
-plot(predY,beersTest[,1])
+cv.error
+plot(cv.error, main = "Cross Validation")
