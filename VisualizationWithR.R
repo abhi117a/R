e599b15774373c305669dd72c453cdbcc68d265b
abhi117a <abhi_9trox@yahoo.co.uk>
2017-03-20 17:30:24 -0500

    Application of different types of visualization in R

---
+++ VisualizationWithR.R
@@ -0,0 +1,42 @@
+library(ggplot2)
+head(mpg)
+str(mpg)
+
+ggplot(mpg) +
+  geom_point(aes(x= displ, y= hwy)) +
+  facet_wrap(~class)
+
+
+
+ggplot(mpg) +
+  geom_point(aes(x= displ, y= hwy)) +
+  facet_wrap(~cyl)
+
+
+
+ggplot(mpg) +
+  geom_point(aes(x= displ, y= hwy)) +
+  facet_wrap(~hwy)
+
+ggplot(mpg,aes(x= displ, y= hwy)) +
+  geom_point() + geom_smooth()
+
+#wigliness of the line is controlled by the span parameter
+
+ggplot(mpg,aes(x= displ, y= hwy)) +
+  geom_point() + geom_smooth(span = 0.2)
+
+ggplot(mpg,aes(x= displ, y= hwy)) +
+  geom_point() + geom_smooth(span = 1)
+
+library("mgcv")
+#if data set is huge then Loess does not work well ie span parameter fails so we use mgcv
+
+ggplot(mpg,aes(x= displ, y= hwy)) +
+  geom_point() + geom_smooth(method = "gam", formula = y~s(x))
+
+#gives best linear fit
+ggplot(mpg,aes(x= displ, y= hwy)) +
+  geom_point() + geom_smooth(method = "lm")
+
+
