62763645ff4473720bddc157140a3be7da325be7
Abhimanyu Rana - a0r00rf <Abhimanyu.Rana@walmart.com>
2017-12-05 15:31:48 -0600

    library dependencies, modifying dataset to fit OneR and implementing OneR

--- HappinessFinder.R
+++ HappinessFinder.R
@@ -1,5 +1,10 @@
 library(tidyverse)
-
+library(OneR)
+library(magrittr)
+library(sos)
+library(caret)
+library(doParallel)
+#library(binr)
 data_16 <- read.table("2016.csv", sep = ",", header = TRUE)
 data_15 <- read.table("2015.csv", sep = ",", header = TRUE)
 
@@ -37,7 +42,79 @@ data_15_16 %>%
 
 data_15_16 <- select(data_15_16, -Happiness.Score) %>%
   mutate(Happiness.Score.l = plyr::revalue(Happiness.Score.l, c("(2.83,4.79]" = "low", "(4.79,5.89]" = "medium", "(5.89,7.59]" = "high")))
-colnames(data_15_16)
-head(data_15_16)
+
+
+#Because there are only 9 features in this small dataset, I want to explore them all individually before modeling. 
+#First, I am plotting the only categorical variable: Region.
+#This plots shows that there are a few regions with very strong biases in happiness: 
+#People in Western Europe, Australia, New Zealand, North America, Latin American and the Caribbean tend to me in the high happiness group, 
+#while people in sub-saharan Africa and Southern Asia tend to be the least happiest.
+
+
+
+data_15_16 %>%
+  ggplot(aes(x = Region, fill = Happiness.Score.l)) +
+  geom_bar(position = "dodge", alpha = 0.7) +
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1),
+        plot.margin = unit(c(0, 0, 0, 1.5), "cm")) +
+  scale_fill_brewer(palette = "Set1")
+
+#The remaining quantitative variables show happiness biases to varying degrees: 
+#e.g. low health and life expectancy is strongly biased towards low happiness, economic factors, 
+#family and freedom show a bias in the same direction, albeit not as strong.
+
+
+data_15_16 %>%
+  gather(x, y, Economy..GDP.per.Capita.:Dystopia.Residual) %>%
+  ggplot(aes(x = y, fill = Happiness.Score.l)) +
+  geom_histogram(alpha = 0.7) +
+  facet_wrap(~ x, scales = "free", ncol = 4) +
+  scale_fill_brewer(palette = "Set1")
+
+#While OneR could also handle categorical data, in this example, I only want to consider the quantitative features to show the differences between 
+#OneR and other machine learning algorithms.
+
+
+data_15_16 <- select(data_15_16, -Region)
+
+#The algorithms I will compare to OneR will be run via the caret package.
+
+# configure multicore
+
+cl <- makeCluster(detectCores())
+registerDoParallel(cl)
+
+#I will also use caret’s createDataPartition() function to partition the data into training (70%) and test sets (30%).
+
+set.seed(42)
+index <- createDataPartition(data_15_16$Happiness.Score.l, p = 0.7, list = FALSE)
+train_data <- data_15_16[index, ]
+test_data  <- data_15_16[-index, ]
+
+#OneR only accepts categorical features. Because we have numerical features, 
+#we need to convert them to factors by splitting them into appropriate bins. 
+#While the original OneR algorithm splits the values into ever smaller factors, this has been changed in 
+#this R-implementation with the argument of preventing overfitting. We can either split the data 
+#into pre-defined numbers of buckets (by length, content or cluster) or we can use the optbin() function to 
+#obtain the optimal number of factors from pairwise logistic regression or information gain.
+
+# default method length
+data_1 <- bin(train_data, nbins = 5, method = "length")
+
+# method content
+data_2 <- bin(train_data, nbins = 5, method = "content")
+
+# method cluster
+data_3 <- bin(train_data, nbins = 3, method = "cluster")
+
+# optimal bin number logistic regression
+data_4 <- optbin(formula = Happiness.Score.l ~., data = train_data, method = "logreg")
+
+# optimal bin number information gain
+data_5 <- optbin(formula = Happiness.Score.l ~., data = train_data, method = "infogain")
+
+
+
+
   
 
