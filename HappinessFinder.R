eb645722a0bad1365deea6b851215fe73e5fd521
Abhimanyu Rana - a0r00rf <Abhimanyu.Rana@walmart.com>
2018-01-08 16:45:32 -0600

    Reupload the file cz I deleted my branch before merging with master cz I am an idiot! Didn't update with comments will ypdate it later

--- HappinessFinder.R
+++ HappinessFinder.R
@@ -4,6 +4,8 @@ library(magrittr)
 library(sos)
 library(caret)
 library(doParallel)
+library(rpart)
+library(rpart.plot)
 #library(binr)
 data_16 <- read.table("2016.csv", sep = ",", header = TRUE)
 data_15 <- read.table("2015.csv", sep = ",", header = TRUE)
@@ -115,6 +117,181 @@ data_5 <- optbin(formula = Happiness.Score.l ~., data = train_data, method = "in
 
 
 
+for (i in 1:5) {
+  data <- get(paste0("data_", i))
+  print(model <- OneR(formula = Happiness.Score.l ~., data = data, verbose = TRUE))
+  assign(paste0("model_", i), model)
+}
+
+
+for (i in 1:5) {
+  model <- get(paste0("model_", i))
+  eval_model(predict(model, test_data), test_data$Happiness.Score.l)
+}
+
+
+for (i in 1:5) {
+  model <- get(paste0("model_", i))
+  pred <- data.frame(model = paste0("model_", i),
+                     sample_id = 1:nrow(test_data),
+                     predict(model, test_data, type = "prob"),
+                     actual = test_data$Happiness.Score.l)
+  pred$prediction <- colnames(pred)[3:5][apply(pred[, 3:5], 1, which.max)]
+  pred$correct <- ifelse(pred$actual == pred$prediction, "correct", "wrong")
+  pred$pred_prob <- NA
+  
+  for (j in 1:nrow(pred)) {
+    pred[j, "pred_prob"] <- max(pred[j, 3:5])
+  }
+  
+  if (i == 1) {
+    pred_df <- pred
+  } else {
+    pred_df <- rbind(pred_df, pred)
+  }
+}
+
+
+set.seed(42)
+fit <- rpart(Happiness.Score.l ~ .,
+             data = train_data,
+             method = "class",
+             control = rpart.control(xval = 10), 
+             parms = list(split = "information"))
+
+rpart.plot(fit, extra = 100)
+
+
+pred <- data.frame(model = "rpart",
+                   sample_id = 1:nrow(test_data),
+                   predict(fit, test_data, type = "prob"),
+                   actual = test_data$Happiness.Score.l)
+pred$prediction <- colnames(pred)[3:5][apply(pred[, 3:5], 1, which.max)]
+pred$correct <- ifelse(pred$actual == pred$prediction, "correct", "wrong")
+pred$pred_prob <- NA
+
+for (j in 1:nrow(pred)) {
+  pred[j, "pred_prob"] <- max(pred[j, 3:5])
+}
+
+
+pred_df_final <- rbind(pred_df,
+                       pred)
+
+
+set.seed(42)
+model_rf <- caret::train(Happiness.Score.l ~ .,
+                         data = train_data,
+                         method = "rf",
+                         trControl = trainControl(method = "repeatedcv", 
+                                                  number = 10, 
+                                                  repeats = 5, 
+                                                  verboseIter = FALSE))
+
+varImp(model_rf)
+
+
+pred <- data.frame(model = "rf",
+                   sample_id = 1:nrow(test_data),
+                   predict(model_rf, test_data, type = "prob"),
+                   actual = test_data$Happiness.Score.l)
+pred$prediction <- colnames(pred)[3:5][apply(pred[, 3:5], 1, which.max)]
+pred$correct <- ifelse(pred$actual == pred$prediction, "correct", "wrong")
+pred$pred_prob <- NA
+
+for (j in 1:nrow(pred)) {
+  pred[j, "pred_prob"] <- max(pred[j, 3:5])
+}
+
+
+pred_df_final <- rbind(pred_df_final,
+                       pred)
+
+
+set.seed(42)
+model_xgb <- caret::train(Happiness.Score.l ~ .,
+                          data = train_data,
+                          method = "xgbTree",
+                          trControl = trainControl(method = "repeatedcv", 
+                                                   number = 10, 
+                                                   repeats = 5, 
+                                                   verboseIter = FALSE))
+
+varImp(model_xgb)
+
+
+pred <- data.frame(model = "xgb",
+                   sample_id = 1:nrow(test_data),
+                   predict(model_xgb, test_data, type = "prob"),
+                   actual = test_data$Happiness.Score.l)
+pred$prediction <- colnames(pred)[3:5][apply(pred[, 3:5], 1, which.max)]
+pred$correct <- ifelse(pred$actual == pred$prediction, "correct", "wrong")
+pred$pred_prob <- NA
+
+for (j in 1:nrow(pred)) {
+  pred[j, "pred_prob"] <- max(pred[j, 3:5])
+}
+pred_df_final <- rbind(pred_df_final,
+                       pred)
+
+
+set.seed(42)
+model_nn <- caret::train(Happiness.Score.l ~ .,
+                         data = train_data,
+                         method = "mlp",
+                         trControl = trainControl(method = "repeatedcv", 
+                                                  number = 10, 
+                                                  repeats = 5, 
+                                                  verboseIter = FALSE))
+
+varImp(model_nn)
+
+
+pred <- data.frame(model = "nn",
+                   sample_id = 1:nrow(test_data),
+                   predict(model_nn, test_data, type = "prob"),
+                   actual = test_data$Happiness.Score.l)
+pred$prediction <- colnames(pred)[3:5][apply(pred[, 3:5], 1, which.max)]
+pred$correct <- ifelse(pred$actual == pred$prediction, "correct", "wrong")
+pred$pred_prob <- NA
+
+for (j in 1:nrow(pred)) {
+  pred[j, "pred_prob"] <- max(pred[j, 3:5])
+}
+pred_df_final <- rbind(pred_df_final,
+                       pred)
+
+
+pred_df_final %>%
+  ggplot(aes(x = actual, y = pred_prob, fill = prediction, color = prediction)) +
+  geom_boxplot(alpha = 0.7) +
+  facet_grid(correct ~ model) +
+  scale_color_brewer(palette = "Set1") +
+  scale_fill_brewer(palette = "Set1")
+
+
+
+pred_df_final %>%
+  group_by(model) %>%
+  dplyr::summarise(correct = sum(correct == "correct")) %>%
+  mutate(accuracy = correct / nrow(test_data)) %>%
+  ggplot(aes(x = model, y = accuracy, fill = model)) +
+  geom_bar(stat = "identity") +
+  scale_fill_brewer(palette = "Set1")
+
+
+pred_df_final %>%
+  group_by(model, prediction) %>%
+  dplyr::summarise(correct = sum(correct == "correct"),
+                   n = n()) %>%
+  mutate(accuracy = correct / n) %>%
+  ggplot(aes(x = model, y = accuracy, fill = prediction)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  scale_fill_brewer(palette = "Set1")
+
+
+
+sessionInfo()
 
   
 
